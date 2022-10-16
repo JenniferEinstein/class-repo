059ac695b7ba2d2339008ae0f35642b1d4b78171
JenniferEinstein <jennifereinstein@gmail.com>
2022-10-16 11:14:49 -0400

    changes - the first time

---
+++ main.js
@@ -0,0 +1,2 @@
+//Jon wants to be a blueberry.
+//Violet. you're turning violet, Violet.
---
+++ script.js
@@ -0,0 +1,3 @@
+//another random file with random stuff so Jon can show us
+//how Git and GitHub work
+//as he says, it doesn't matter
